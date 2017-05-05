a785483dd2e4445770c6876370a9a4a17d0de537
VoidPragma <voidpragma@outlook.jp>
2017-05-05 16:17:38 +0900

    Add files via upload

--- helloworld/src/helloworld/MainPanel.java
+++ helloworld/src/helloworld/MainPanel.java
@@ -1,27 +1,63 @@
 package helloworld;
 
-import java.awt.Dimension;
-import java.awt.Graphics;
-import javax.swing.JPanel;
+import java.awt.*;
+import javax.swing.*;
 
-public class MainPanel extends JPanel {
+public class MainPanel extends JPanel implements Runnable {
 	//パネルサイズ
 	private static final int WIDTH = 240;
 	private static final int HEIGHT = 240;
-	private String str;
+	//ボールの大きさ、位置、速度
+	private static final int SIZE = 10;
+	private int x;
+	private int y;
+	private int vx;
+	private int vy;
+	//アニメーション用スレッド
+	private Thread thread;
 	
 	public MainPanel(){
 		//パネルの推奨サイズを設定、pack()する時に必要
 		setPreferredSize(new Dimension(WIDTH,HEIGHT));
 		//変数などの初期化
-		str = "Hello World";
+		x = 100;
+		y = 100;
+		vx = 1;
+		vy = 1;
+		
+		//スレッド起動
+		thread = new Thread(this);
+		thread.start();
 	}
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		
 		//盤面を描いたり、フィールドを描いたりする
-		g.drawString(str, 20, 50);
+		g.setColor(Color.BLUE);
+		g.fillOval(x - SIZE, y - SIZE, SIZE, SIZE);
+	}
+	
+	//メインループ
+	
+	public void run(){
+		//プログラムが終了するまでフレーム処理を繰り返す
+		while(true){
+			x += vx;
+			y += vy;
+			
+			//再描写
+			repaint();
+			
+			//20ミリ秒だけ休止
+			try{
+				Thread.sleep(20);
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
+			
+			
+		}
 	}
 
 }
